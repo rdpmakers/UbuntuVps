bfb0d57f7792c9d50b8e9c79e38c76c6e81f39a6
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-09-01 18:41:01 +0700

    Update CRD.sh

--- CRD.sh
+++ CRD.sh
@@ -4,12 +4,14 @@ echo "============================="
 echo "   updating, please wait"
 echo "============================="
 sudo apt-get update
+sleep 5
 clear
 clear
 echo "============================="
 echo "       installing GUI"
 echo "============================="
 sudo apt install ubuntu-desktop -y
+sleep 5
 clear
 clear
 echo "============================="
@@ -17,6 +19,7 @@ echo "      installing CRD"
 echo "============================="
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
+sleep 5
 clear
 clear
 echo "============================="
@@ -24,6 +27,7 @@ echo "  installing chrome browser"
 echo "============================="
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+sleep 5
 clear
 clear
 echo "============================="
@@ -32,6 +36,7 @@ echo "============================="
 sudo adduser ubuntu --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "ubuntu:ubuntu" | sudo chpasswd
 sudo usermod -aG sudo,adm ubuntu
+sleep 5
 clear
 clear
 echo "========================================================="
