41f9c99ae0b120bdd19a34fcb9e9cab9a5e09b83
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-09-03 13:48:37 +0700

    Create xfce.sh

---
+++ xfce.sh
@@ -0,0 +1,11 @@
+sudo adduser ubuntu --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "ubuntu:ubuntu" | sudo chpasswd
+sudo usermod -aG sudo,adm ubuntu
+clear
+sudo apt install xfce4 xfce4-goodies -y
+clear
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+clear
+sudo apt install tightvncserver -y
+vncserver
