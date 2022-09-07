e3a40b5689e621615eb020ed1c19146c12389ecb
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-09-07 19:18:33 +0700

    Update VNC.sh

--- VNC.sh
+++ VNC.sh
@@ -1,1 +1,17 @@
-
+clear
+clear
+echo "============================="
+echo "         adding user"
+echo "============================="
+sudo adduser ubuntu --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "ubuntu:ubuntu" | sudo chpasswd
+sudo usermod -aG sudo,adm ubuntu
+clear
+clear
+echo "============================="
+echo "       installing xfce"
+echo "============================="
+sudo apt update
+sudo apt install xfce4 xfce4-goodies -y
+clear
+clear
