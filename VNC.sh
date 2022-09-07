d868349006c481ba5b664a673e7343add42cef28
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-09-07 19:19:34 +0700

    Update VNC.sh

--- VNC.sh
+++ VNC.sh
@@ -6,6 +6,8 @@ echo "============================="
 sudo adduser ubuntu --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "ubuntu:ubuntu" | sudo chpasswd
 sudo usermod -aG sudo,adm ubuntu
+su ubuntu
+cd
 clear
 clear
 echo "============================="
@@ -13,5 +15,11 @@ echo "       installing xfce"
 echo "============================="
 sudo apt update
 sudo apt install xfce4 xfce4-goodies -y
+clear
+clear
+echo "============================="
+echo "     installing tightvnc"
+echo "============================="
+sudo apt install tightvncserver
 clear
 clear
